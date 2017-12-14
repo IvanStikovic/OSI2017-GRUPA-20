6c4f00e06cf664f9e50353bcf9d499850ab92aa2
IvanStikovic <stikovicivan@gmail.com>
2017-12-14 13:37:39 +0100

    Sistem funkcije - Uradjene neke

--- Project/Sistem.c
+++ Project/Sistem.c
@@ -1,1 +1,33 @@
 #include "Sistem.h"
+
+void sistemStart()
+{
+    loading();
+    if(login())
+        optionMeni();
+    else
+        return;
+}
+
+bool login()
+{
+    char c;
+    do
+    {
+    printf("Unesite username :");
+    char username[20],sifra[20];
+    scanf("%s",pom);
+    printf("\nUnesite sifru :");
+    scanf("%s",sifra);
+    if(provjeriNalog(username,sifra))
+        return true;
+    else
+        {
+            printf("\nPogresan username ili sifra !!!");
+            printf("\nZa ponovo logovanje na sistem unesite 1.");
+            printf("\nZa prekid prijavljivanja unesite 0.");
+            scanf(" %c",&c);
+        }
+    }while(c);
+    return false;
+}
--- Project/Sistem.h
+++ Project/Sistem.h
@@ -3,16 +3,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void sistemStart();
-bool login();
-bool provjeriNalog(char*,char*);
+
+
+void sistemStart(); //URADJENO DZ
+//PRIJAVA NA SISTEM
+bool login(); //URADJEDNO DZ
+bool provjeriNalog(char*,char*); //to do -IGAC
 void pristupNalog(); // Ove tri ispod funkcije ce se moci samo pozvati iz ove (1)
 void dodajNalog();//  1
 void brisiNalog();//  1
 void azurirajNalog();// 1
 
+//BIRANJE OPCIJA
+void optionMenu();
 
 
+
+////GRAFICKI INTERFEJS
+void loading();
+            //zvjezdice pri unosu sifre
 
 
 #endif // SISTEM_H_INCLUDED
