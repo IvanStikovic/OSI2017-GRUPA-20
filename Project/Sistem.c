859436c7c8214dbea8da708612bb69b6c668d5e1
Igor Stanivukovic <IGOR@10.99.170.221>
2017-12-15 17:56:01 -0800

    Funkcija azurirajNalog Nedovrseno,nastavljam sutra poslije konsultacija sa vodjom projekta!

--- Project/Sistem.c
+++ Project/Sistem.c
@@ -40,7 +40,7 @@ bool provjeriNalog(char* usrnm ,char* sfr)
     FILE *dat;
     if(dat=fopen("RADNICI.txt","r"))
     {
-        provjera=fscanf(dat,"%s %s %c %s %s %lf",username,pin,&radno_mjesto,ime,prezime,&plata);
+        provjera=fscanf(dat,"%s %s %lf %s %s %c",ime,prezime,&plata,username,pin,&radno_mjesto);
         while(provjera==6)
         {
             if((strcmp(username,usrnm)==0) && (strcmp(pin,sfr)==0))
@@ -48,7 +48,7 @@ bool provjeriNalog(char* usrnm ,char* sfr)
                 fclose(dat);
                 return true;
             }
-            provjera=fscanf(dat,"%s %s %c %s %s %lf",username,pin,&radno_mjesto,ime,prezime,&plata);
+            provjera=fscanf(dat,"%s %s %lf %s %s %c",ime,prezime,&plata,username,pin,&radno_mjesto);
 
         }
         fclose(dat);
@@ -109,5 +109,25 @@ void sacuvajNalog(RADNIK *rad)
     else
         printf("Nalog nije uspjesno kreiran i sacuvan, greska prilikom cuvanja naloga u bazi.\n");
 }
+
+void azurirajNalog()
+{
+    FILE *dat;
+    int br_radnika=1,provjera;
+    char pom[20],ime[20],prezime[20],username[20],pin[5],radno_mjesto;
+    double plata;
+    printf("Unesite username naloga koji zelite da azurirate!\n");
+    scanf("%s",pom);
+    if(dat=fopen("RADNICI.txt","r"))
+    {
+        provjera=fscanf(dat,"%s %s %lf %s %s %c",ime,prezime,&plata,username,pin,&radno_mjesto);
+        while(provjera==6)
+        {
+            provjera=fscanf(dat,"%s %s %lf %s %s %c",ime,prezime,&plata,username,pin,&radno_mjesto);
+            br_radnika++;
+        }
+        fclose(dat);
+    }// NEDOVRSENO NASTAVLJAM SUTRA
+}//#ByIgorS
 
 
