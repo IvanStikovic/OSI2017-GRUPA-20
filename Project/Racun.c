bb855797dc3f2a1bb29c97cda0bba1800693bce6
IvanStikovic <stikovicivan@gmail.com>
2017-12-19 16:57:35 +0100

    Prototipi - Racun(format1)

--- Project/Racun.c
+++ Project/Racun.c
@@ -1,12 +1,22 @@
 #include "Racun.h"
 
-void ucitajRacune()
+void uveziRacune()
 {
-    FILE* dat=fopen("RACUNI.txt","r");
-    if(dat)
-    {
-
-    }
+    char c;
+    RACUN* pom;
+    c=(char*)calloc(1,1);
+    for(int i=1; i<=5; i++)
+        if(validanRacun(argv[i]))
+        {
+            if(pom=format1(datoteka[i],c))
+                sacuvajRacun(pom,c);
+            else if(pom=format2(datoteka[i],c))
+                sacuvajRacun(pom,c);
+            else if(pom=format3(datoteka[i],c))
+                sacuvajRacun(pom,c);
+            else (pom=format4(datoteka[i],c))
+                sacuvajRacun(pom,c);
+        }
 }
 
 RACUN* kreirajRacun()
@@ -22,3 +32,44 @@ void dodajProizvod(RACUN* racun,PROIZVOD* proizvod)
     racun->proizvodi[racun->n].naziv=(char*)calloc(strlen(proizvod->naziv)+1,sizeof(char));
     strcpy(racun->proizvodi[racun->n].naziv,proizvod->naziv);
 }
+
+RACUN* format1(char* file,char* c)
+{
+    FILE* dat;
+    if(dat=fopen(file,"r"))
+    {
+        RACUN* pomRacun=kreirajRacun();
+        pomRacun.n=5;
+        char pomString[150];
+        int br=0;
+        char naziv[30];
+        double kolicina,cijena,ukupno;
+        pomRacun.proizvodi=(PROIZVOD*)calloc(pomRacun.n,sizeof(PROIZVOD));
+        pomRacun.kolicina=(double*)calloc(pomRacun.n,sizeof(double));
+        for(i=0; i<6; i++)
+            fgets(pomString,150,dat);
+        while(fscanf("%s %lf %lf %lf",naziv,&kolicina,&cijena,&ukupno)==4)
+        {
+            if(br==(pomRacun.n-1))
+            {
+                pomRacun.proizvodi=realloc(pomRacun.proizvodi,n*2*sizeof(PROIZVOD*));
+                pomRacun.kolicina=realloc(pomRacun.kolicina,n*2*sizeof(double));
+                pomRacun.n*=pomRacun.n;
+            }
+            pomRacun.proizvodi[br]=kreirajProizvod(naziv,cijena);//POVRATNI TIP MODIFIKOVATI ILI NACI DRUGO RJESENJE
+
+            int sum=0;
+            for(i=0; i<pomRacun.n; i++)
+                sum+=pomRacun.proizvodi[i].cijena*pomRacun.kolicina[i];
+
+            fgets(pomString,150,dat);
+            fscanf("%s %d",pomString,ukupno);
+            if(ukupno==sum)
+                return pomRacun;
+            else
+            {   obrisiRacun(pomRacun);
+                return 0;
+            }
+        }
+        else return 0;
+    }
--- Project/Racun.h
+++ Project/Racun.h
@@ -1,6 +1,7 @@
 #ifndef RACUN_H_INCLUDED
 #define RACUN_H_INCLUDED
 #include <stdio.h>
+
 #include "Proizvod.h"
 
 typedef struct racun
@@ -12,14 +13,19 @@ typedef struct racun
 } RACUN;
 
 void uveziRacune(); //UCITAVA RACUNE I RASPOREDJUJE IH U ODNOSU NA TO DA LI SU ULAZNI ILI IZLAZNI
-void sacuvajRacun(char); // SNIMA RACUN U DATOTEKU (char da se vidi da li je ulazni ili izlazni)
+void sacuvajRacun(RACUN*,char); // SNIMA RACUN U DATOTEKU (char da se vidi da li je ulazni ili izlazni)
 RACUN* ucitajRacune(char); // VRACA DINAMICKI ALOCIRAN NIZ RACUNA (char opet ista svrha)
 RACUN* kreirajRacun();
+int validanRacun(char*);
 void dodajProizvod(RACUN*,PROIZVOD*);
 void izmjeniRacun(RACUN*);
-void obrisiRacun();
+void obrisiRacun(RACUN*);
 
-
+//FORMATI RACUNA
+RACUN* format1(char*,char*);
+RACUN* format2(char*,char*);
+RACUN* format3(char*,char*);
+RACUN* format4(char*,char*);
 
 
 #endif // RACUN_H_INCLUDED
